3471db90c34061a4ef2921e7e4145d0f7e3929fb
joealbert5 <joealbert5@yahoo.com>
2018-01-24 02:33:29 -0800

    end of the night
    end of night

--- lab1.m
+++ lab1.m
@@ -1,29 +1,31 @@
 %lab1
 
 R = zeros(4,4);
+L2 = 12;
+L3 = 6;
+L4 = 9;
 R01 = zeros(4,4);
 R12 = zeros(4,4);
 R23 = zeros(4,4);
 R34 = zeros(4,4);
 R45 = zeros(4,4);
-thetas = zeros(1,5);
-alphas = zeros(1,5);
-ds = zeros(1,5);
-as = zeros(1,5);
+alphas = [0,90,90,90,0];
+ds = [0,0,L3,0,L4];
+as = [0,0,L2,0,0];
 
 for n = 1:1:5
-	R(1,1) = cos(thetas(n));
-	R(1,2) = -1*sin(thetas(n));
+	R(1,1) = cosd(thetas(n));
+	R(1,2) = -1*sind(thetas(n));
 	R(1,3) = 0;
-	R(1,4) = as(n-1);
-	R(2,1) = sin(thetas(n))*cos(alphas(n-1));
-	R(2,2) = cos(thetas(n))*cos(alphas(n-1));
-	R(2,3) = -1*sin(alphas(n-1));
-	R(2,4) = -1*ds(n)*sin(alphas(n-1));
-	R(3,1) = sin(thetas(n))*sin(alphas(n-1));
-	R(3,2) = cos(thetas(n))*sin(alphas(n-1));
-	R(3,3) = cos(alphas(n-1));
-	R(3,4) = ds(n)*cos(alphas(n-1));
+	R(1,4) = as(n);
+	R(2,1) = sind(thetas(n))*cosd(alphas(n));
+	R(2,2) = cosd(thetas(n))*cosd(alphas(n));
+	R(2,3) = -1*sind(alphas(n));
+	R(2,4) = -1*ds(n)*sind(alphas(n));
+	R(3,1) = sind(thetas(n))*sind(alphas(n));
+	R(3,2) = cosd(thetas(n))*sind(alphas(n));
+	R(3,3) = cosd(alphas(n));
+	R(3,4) = ds(n)*cosd(alphas(n));
 	R(4,1) = 0;
 	R(4,2) = 0;
 	R(4,3) = 0;
@@ -39,4 +41,7 @@ for n = 1:1:5
 		R34 = R;
 	elseif n == 5
 		R45 = R;
+    end
 end
+
+res = R01*R12*R23*R34*R45
