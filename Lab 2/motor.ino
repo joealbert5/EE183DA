5b8bfa8d1183f65741aff808c9430e427aa18c9e
byao22 <33672050+byao22@users.noreply.github.com>
2018-02-06 20:38:57 -0800

    Add files via upload

---
+++ Lab 2/motor.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+  servo.attach(16);
+  servo.write(0);
+  delay(1000);
+}
+
+void loop() {
+  while(){
+   servo.write(90);
+   delay(1000);
+   servo.write(0);
+   delay(1000);
+  }
+}
+
+
+// this is what i did to get the motor to stop after a set # of iterations 
+/*
+int i = 0;
+void loop() {
+  i++;
+  if(i < 5){
+   servo.write(180);
+   delay(1000);
+   servo.write(0);
+   delay(1000);
+  }
+  if (i>5){
+    servo.write(0);
+  }
+}*/
