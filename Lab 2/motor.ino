34307a04fa8e48454b8f562a98ab32969b653327
byao22 <33672050+byao22@users.noreply.github.com>
2018-02-08 01:56:24 -0800

    Add files via upload

--- Lab 2/motor.ino
+++ Lab 2/motor.ino
@@ -5,31 +5,74 @@ Servo servo;
 void setup() {
   servo.attach(16);
   servo.write(0);
-  delay(1000);
+  pinMode(D4, INPUT);
+  Serial.begin(115200);
 }
 
+int i=0;
 void loop() {
-  while(){
-   servo.write(90);
-   delay(1000);
-   servo.write(0);
-   delay(1000);
-  }
-}
-//different tempos will be same code with different delays
-
-// this is what i did to get the motor to stop after a set # of iterations 
-/*
-int i = 0;
-void loop() {
-  i++;
-  if(i < 5){
-   servo.write(180);
-   delay(1000);
-   servo.write(0);
-   delay(1000);
-  }
-  if (i>5){
+ int ctsValue = digitalRead(D4);
+ Serial.println(ctsValue);
+ if(ctsValue == HIGH){
+   i++;
+   if (i<2){
+    servo.write(90);
+    delay(500);
     servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(250);
+    servo.write(0);
+    delay(500);
+    servo.write(90);
+    delay(250);
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(500);
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(500);
   }
-}*/
+  if(i<100){ 
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(250);
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(500);
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(250);
+    servo.write(0);
+    delay(500);
+    servo.write(90);
+    delay(250);
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(500);
+    servo.write(0);
+    delay(250);
+    servo.write(90);
+    delay(500);
+  }
+   if(i>99){
+    servo.write(90);
+   }
+ }
+ if (ctsValue = LOW){
+    servo.write(90);
+    delay(100);
+    servo.write(0);
+    delay(100);
+  } 
+}
+
+
+
+
