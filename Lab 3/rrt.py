95a27e0ecd999e8aea275c4af59549f84dff1ccc
Joe Fague <joealbert5@yahoo.com>
2018-03-07 01:08:46 -0800

    cleaner rrt

--- Lab 3/rrt.py
+++ Lab 3/rrt.py
@@ -8,18 +8,6 @@ class Node:
         self.coord = coord
         self.next = []
 
-    def getCoord(self):
-        return self.coord
-
-    def getNext(self):
-        return self.next
-
-    def setCoord(self,newdata):
-        self.coord = newdata
-
-    def setNext(self,newnext):
-        self.next.append(newnext)
-
     def printCoords(self):
     	print("coords: " + str(self.coord))
 
@@ -34,18 +22,12 @@ class Node:
     def sizeOfNexts(self):
     	return len(self.next)
 
-
-MAX_X = 100
-MAX_Y = 100
-ERR_RADIUS = 4
-
 def radius(x, y):
 	x2 = math.pow(x,2)
 	y2 = math.pow(y,2)
 	return math.sqrt(x2 + y2)
 
 def bfsFindNear(head, qrand):
-	#printRRT(head)
 	queue = [head]
 	currminDiff = 9999
 	currNear = Node([0,0])
@@ -74,12 +56,11 @@ def bfsFindAndSetNear(start, qnear, qnew):
 			if node != None:
 				queue.append(node)
 		if curr.equals(qnear):
-			curr.next.append(qnew)	#try setNext?
+			curr.next.append(qnew)
 			return True
 	return False
 
 def bfsFindIfPathExistTo(start, dest):
-	print("start is " + str(start))
 	queue = [(start, [(start, 20)])]
 	destx = dest.coord[0]
 	desty = dest.coord[1]
@@ -91,13 +72,8 @@ def bfsFindIfPathExistTo(start, dest):
 		nexts = curr[0].next
 		for node in nexts:
 			if node != None:
-				print("i is " + str(i))
-				print("currNode is " + str(currNode.coord))
 				newPath = list(currPath)
 				newPath.append((currNode, i))
-				print("curr path is ")
-				for n in newPath:
-					print(n[0].coord)
 				tup = (node, newPath)
 				queue.append(tup)
 		currx = curr[0].coord[0]
@@ -107,82 +83,21 @@ def bfsFindIfPathExistTo(start, dest):
 			#return path to curr
 			return curr
 	return None
-
-def bfsFindPath(start, dest):
-	queue = [start]
-	graph = {}
-	graphcoord = {}
-	destx = dest.coord[0]
-	desty = dest.coord[1]
-	while len(queue) > 0:
-		curr = queue.pop(0)
-		children = []
-		nexts = curr.next
-		for node in nexts:
-			if node != None:
-				queue.append(node)
-				children.append(node)
-		currx = curr.coord[0]
-		curry = curr.coord[1]
-		graph[curr] = set(children)
-		graphcoord[(currx,curry)] = children
-		if abs(currx - destx) < ERR_RADIUS and abs(curry - desty) < ERR_RADIUS:
-			print(graphcoord)
-			q = [(start, [start])]
-			while q:
-				(vertex, path) = q.pop(0)
-				for next in graphcoord[vertex] - set(path):
-					if next == dest:
-						print("found path ")
-						print(path)
-						return path + [next]
-					else:
-						q.append((next, path + [next]))
-	return None
 	
 def moveInc(qnear, qrand, dq):
 	qrx = qrand[0]
 	qry = qrand[1]
 	mag = radius(qrx, qry)
-	qrxnorm = qrx/mag
-	qrynorm = qry/mag
-	qnearValx = qnear.coord[0]
-	qnearValy = qnear.coord[1]
-	qnearValx += qrxnorm*dq
-	qnearValy += qrynorm*dq
-	return Node([qnearValx, qnearValy])	#returns qnew
-
-#start = Node([0,0])
-def rrt(init):
-	start = Node(init)
-	qnear = Node([0,0])
-	qnew = Node([0,0])
-	for k in range(1, K):
-		qrand = [r.randint(-1*MAX_X,MAX_X),r.randint(-1*MAX_Y,MAX_Y)]
-		qnear = bfsFindNear(start, qrand)	#returns a Node
-		qnew = moveInc(qnear, qrand, 5)
-		if not bfsFindAndSetNear(start, qnear, qnew):
-			print("couldn't find qnear")
-
-	return start
-
-def printRRT(head):
-	queue = [head]
-	i = 0
-	j = 0
-	while len(queue) > 0:
-		curr = queue.pop(0)
-		nexts = curr.next
-		for node in nexts:
-			if node != None:
-				queue.append(node)
-				j += 1
-		print(curr.coord)
-		i += 1
-		print("j is :" + str(j))
-		j = 0
-	#head.printNexts()
-	print("i is :" + str(i))
+	if mag != 0:
+		qrxnorm = qrx/mag
+		qrynorm = qry/mag
+		qnearValx = qnear.coord[0]
+		qnearValy = qnear.coord[1]
+		qnearValx += qrxnorm*dq
+		qnearValy += qrynorm*dq
+		return Node([qnearValx, qnearValy])	#returns qnew
+	else:
+		return qnear
 
 def getDataRRT(head):
 	data = {"x":[], "y":[]}
@@ -199,8 +114,8 @@ def getDataRRT(head):
 
 def graph(x, y, xd, yd, xp, yp, color):
 	pyplot.scatter(x, y, s=8)
-	pyplot.scatter(xd, yd, s=32, c='y')
-	pyplot.scatter(100, 100, s=32, c='g')
+	pyplot.scatter(xd, yd, s=64, c='g')
+	pyplot.scatter(100, 100, s=64, c='g')
 	pyplot.scatter(xp, yp, c=color)
 	pyplot.plot(xp, yp, c='c')
 	pyplot.show()
@@ -209,27 +124,44 @@ def graph2(x, y):
 	pyplot.scatter(x, y, s=8)
 	pyplot.show()
 
+def rrt(init):
+	start = Node(init)
+	qnear = Node([0,0])
+	qnew = Node([0,0])
+	for k in range(1, K):
+		qrand = [r.randint(-1*MAX_X,MAX_X),r.randint(-1*MAX_Y,MAX_Y)]
+		qnear = bfsFindNear(start, qrand)	#returns a Node
+		qnew = moveInc(qnear, qrand, 5)
+		if not bfsFindAndSetNear(start, qnear, qnew):
+			print("couldn't find qnear")
+	return start
 
+################################################
+##                  Main                      ##
+################################################
 K = 1000
-#printRRT(rrt([100,100]))
-top = rrt([100,100])
-goal = Node([0,0])
-res = getDataRRT(top)
-x = res['x']
-y = res['y']
+MAX_X = 100
+MAX_Y = 100
+ERR_RADIUS = 4
+start = [100,100]
+end = [0,0]
+
+top = rrt(start)		#makes RRT graph, returns head node
+goal = Node(end)		#goal node
+res = getDataRRT(top)	#gets xy coordinates, seperates them into arrays
+x = res['x']			#x coordinates of RRT graph
+y = res['y']			#y coordinates of RRT graph
 #graph2(x, y)
-dest = bfsFindIfPathExistTo(top, goal)
+dest = bfsFindIfPathExistTo(top, goal)	#determines if a path to goal exists
 if dest != None:
-	print(dest[0])
-	print(dest[1])
 	x = []
 	y = []
 	c = []
-	res = getDataRRT(top)
+	res = getDataRRT(top)	#gets xy coordinates for a short path from head to goal node
 	for node in dest[1]:
-		x.append(node[0].coord[0])
-		y.append(node[0].coord[1])
-		c.append(node[1])
-	graph(res['x'], res['y'], 50, 50, x, y, c)
+		x.append(node[0].coord[0])	#x coordinates of shortest path graph
+		y.append(node[0].coord[1])	#y coordinates of shortest path graph
+		c.append(node[1])			#colors each node along path
+	graph(res['x'], res['y'], end[0], end[1], x, y, c)
 else:
-	print("dest returned None")
+	print("dest returned None, no path found.  Re-run or change some constant parameters")
