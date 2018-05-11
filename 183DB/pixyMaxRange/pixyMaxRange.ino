c271d58876217c5ca1370f2ef47be0b2d04e80f0
Joe Fague <joealbert5@yahoo.com>
2018-05-11 16:14:44 -0700

    Add files via upload

--- 183DB/pixyMaxRange/pixyMaxRange.ino
+++ 183DB/pixyMaxRange/pixyMaxRange.ino
@@ -4,28 +4,31 @@
 #include <PixyUART.h>
 #include <TPixy.h>
 #include <SPI.h>
+#include "ballsearch2.h"
 
 uint16_t blocks;
 uint16_t old_blocks;
-const int BUTTON = D0;
+//const int BUTTON = D0;
 int buttonState = 0;
 int32_t prevArea = 0;
 
 PixySPI_SS pixy(10);
+Ballsearch b(make_tuple(0,0));
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(BUTTON, INPUT);
+  //pinMode(BUTTON, INPUT);
   Serial.println("pinmocde");
-  pixy.init();
+  //pixy.init();
   Serial.println("pixy init");
 }
 
 void loop() {
   //Serial.println("begin loop");
-  buttonState = digitalRead(BUTTON);
-  if(buttonState == HIGH){
+  //buttonState = digitalRead(BUTTON);
+  //if(buttonState == HIGH){
+  /*
     Serial.println("button pressed");
     uint16_t blocks = pixy.getBlocks();
     if (blocks > 0){
@@ -42,7 +45,11 @@ void loop() {
       prevArea = area;
     }
     else
-      Serial.println("blocks not > 0");
+      Serial.println("blocks not > 0");*/
+  int count = b.search("L");
+  Serial.print("FINISHED.  COUNT IS ");
+  Serial.println(count);
+  
   delay(200);
-  }
+  //}
 }
